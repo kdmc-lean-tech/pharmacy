19d2be9d17f2cecf4ff9aa495d3bdf201f06d5df
Kevin Melan <kmelan@lean-tech.io>
2020-06-29 19:35:36 -0500

    Added Bootstrap 4

--- src/app/layouts/main-layout/main-layout-routing.module.ts
+++ src/app/layouts/main-layout/main-layout-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './main-layout.component';
+import { AuthGuard } from '../../core/guards/auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,18 +11,14 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        pathMatch: 'full',
-        redirectTo: 'landing'
-      },
-      {
-        path: 'landing',
         loadChildren: () => import('../../main/landing/landing.module')
           .then(m => m.LandingModule)
       },
       {
         path: 'admin',
         loadChildren: () => import('../../main/admin/admin.module')
-          .then(m => m.AdminModule)
+          .then(m => m.AdminModule),
+        canActivate: [AuthGuard]
       }
     ]
   }
